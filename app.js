f8050e5ecc6d2fbed1ffd8c804ca09cfc98aab35
Charles Best <charlesbest8@gmail.com>
2020-05-29 02:08:13 +0100

    quick fix

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const postRoutes = require('./routes/post');
 require('dotenv').config();
-console.log("env", process.env.MONGO_URI)
-//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
  //middleWares
 app.use(morgan('dev'));
